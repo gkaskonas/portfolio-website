4b7e81366dfd132a32e59c7f50ffb8de10b35425
Giedrius Kaskonas <humorsome_tethered@aleeas.com>
2021-05-01 10:03:52 +0100

    Add new stuff

---
+++ scripts/aws-cli-assumerole.sh
@@ -0,0 +1,18 @@
+unset  AWS_SESSION_TOKEN
+
+
+if [ $CIRCLE_BRANCH == "main" ]
+then
+ temp_role=$(aws sts assume-role \
+                    --role-arn "arn:aws:iam::119184259962:role/PipelineRole" \
+                    --role-session-name "circleci-prod")
+else
+ temp_role=$(aws sts assume-role \
+                    --role-arn "arn:aws:iam::404319983256:role/PipelineRole" \
+                    --role-session-name "circleci-dev")
+fi
+
+
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
