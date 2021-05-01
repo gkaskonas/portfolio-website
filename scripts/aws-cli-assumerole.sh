4ab195e2c14403f8019673413b77529fa688c942
Giedrius Kaskonas <57504623+gkaskonas@users.noreply.github.com>
2021-05-01 22:42:08 +0100

    Update title (#1)
    * Update title
    
    * Remove sudo
    
    * add test
    
    * fix if statement
    
    * add region
    
    * Fix jq
    
    * Add restore cache
    
    * Fix cache path
    
    * test2
    
    * test
    
    Co-authored-by: Giedrius Kaskonas <humorsome_tethered@aleeas.com>

--- scripts/aws-cli-assumerole.sh
+++ scripts/aws-cli-assumerole.sh
@@ -1,11 +1,16 @@
 unset  AWS_SESSION_TOKEN
 
 
-if [ $ENVIRONMENT == "main" ]
+if [ $ENVIRONMENT == "prod" ]
 then
  temp_role=$(aws sts assume-role \
                     --role-arn "arn:aws:iam::119184259962:role/PipelineRole" \
                     --role-session-name "circleci-prod")
+elif [ $ENVIRONMENT == "test" ]
+then
+ temp_role=$(aws sts assume-role \
+                    --role-arn "arn:aws:iam::380477309410:role/PipelineRole" \
+                    --role-session-name "circleci-test")
 else
  temp_role=$(aws sts assume-role \
                     --role-arn "arn:aws:iam::404319983256:role/PipelineRole" \
@@ -13,6 +18,6 @@ else
 fi
 
 
-export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
-export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
-export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
+echo export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs) >> $BASH_ENV
+echo export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs) >> $BASH_ENV
+echo export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs) >> $BASH_ENV
